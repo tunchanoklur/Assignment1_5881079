430232814ebfc3990b6847fe0465c395ff7d748d
kikk <kikpanda@gmail.com>
2019-06-16 12:46:37 +0700

    install semantic ui and axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import SuiVue from 'semantic-ui-vue';
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.use(SuiVue);
+
+// axios.defaults.withCredentials = true
+// axios.defaults.baseURL = 'http://localhost:3000'
 
 /* eslint-disable no-new */
 new Vue({
